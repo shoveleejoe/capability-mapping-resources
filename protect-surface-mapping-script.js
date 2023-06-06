7037cdcaa4ee92b1fec370aebbbdf73a34c21bc9
Jon King <shoveleejoe@gmail.com>
2023-06-05 23:53:27 -0700

    20230605

--- protect-surface-mapping-script.js
+++ protect-surface-mapping-script.js
@@ -1,80 +1,74 @@
-document.getElementById('json-url-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    var url = document.getElementById('json-url').value;
-    
-    // Fetch the JSON data
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            generateCards('panel1', 'asset', data);
-            generateCards('panel2', 'control', data);
-            generateCards('panel3', 'technique', data);
-        })
-        .catch(error => console.error('Error:', error));
-});
-
-function generateCards(panelId, cardType, data) {
-    try {
-        // Get the panel element
-        var panel = document.getElementById(panelId);
-
-        // Get the card container
-        var gridContainer = panel.getElementsByClassName('grid-container')[0];
-
-        // For each card of the specified type in the data
-        for (var i = 0; i < data.cards.length; i++) {
-            if (data.cards[i].cardType === cardType) {
-                try {
-                    // Create the card
-                    var card = document.createElement('div');
-                    card.className = 'card ' + cardType + '-card';
-
-                    // Create the icon
-                    var icon = document.createElement('img');
-                    icon.src = data.cards[i].iconPath;
-                    icon.alt = cardType.charAt(0).toUpperCase() + cardType.slice(1) + ' Icon';
-                    card.appendChild(icon);
-
-                    // Create the display name
-                    var name = document.createElement('h2');
-                    name.textContent = data.cards[i].displayValue;
-                    card.appendChild(name);
-
-                    // Create additional elements based on the card type
-                    if (cardType === 'asset') {
-                        var environment = document.createElement('p');
-                        environment.textContent = 'Environment: ' + data.cards[i].environment;
-                        card.appendChild(environment);
-                    } else if (cardType === 'control') {
-                        var description = document.createElement('p');
-                        description.textContent = 'Description: ' + data.cards[i].controlDescription;
-                        card.appendChild(description);
-
-                        // Add more control-specific data as per your needs...
-                    } else if (cardType === 'technique') {
-                        var id = document.createElement('p');
-                        id.textContent = 'Technique ID: ' + data.cards[i].techniqueID;
-                        card.appendChild(id);
-
-                        var techniqueName = document.createElement('p');
-                        techniqueName.textContent = 'Technique Name: ' + data.cards[i].techniqueName;
-                        card.appendChild(techniqueName);
-
-                        var tactic = document.createElement('p');
-                        tactic.textContent = 'Tactic: ' + data.cards[i].tactic;
-                        card.appendChild(tactic);
-                    }
-
-                    // Add the card to the grid container
-                    gridContainer.appendChild(card);
-                } catch (error) {
-                    console.error('Error creating card:', error);
-                }
-            }
-        }
-    } catch (error) {
-        console.error('Error in generateCards:', error);
+// Utility function to load JSON
+function loadJSON(url, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', url, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(JSON.parse(xobj.responseText));
+      } else {
+        console.error("Error loading JSON");
+      }
+    };
+    xobj.send(null);
+  }
+  
+  // Utility function to create a card
+  function createCard(data, type) {
+    var card = document.createElement('div');
+    card.className = 'card';
+    card.textContent = data.displayValue;
+  
+    // Set the card's grid row and column based on its data
+    var row, column;
+    if (type === 'asset') {
+      row = data.environment;
+      column = data.assetCategory;
+    } else if (type === 'control') {
+      row = data.controlFunction;
+      column = data.assetCategory;
+    } else if (type === 'technique') {
+      row = data.techniqueTactic;
+      column = data.assetCategory;
     }
-}
-
+    card.style.gridColumn = column;
+    card.style.gridRow = row;
+  
+    return card;
+  }
+  
+  // Event listener for JSON form submission
+  document.getElementById('json-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var url = document.getElementById('json-url').value;
+    loadJSON(url, function(data) {
+      // Clear any existing cards
+      var panels = document.querySelectorAll('.panel');
+      panels.forEach(function(panel) {
+        while (panel.firstChild) {
+          panel.removeChild(panel.firstChild);
+        }
+      });
+  
+      // Check if the data for assets, controls, and techniques are defined
+      if(data.assets && Array.isArray(data.assets)) {
+        data.assets.forEach(function(assetData) {
+          var card = createCard(assetData, 'asset');
+          document.getElementById('panel1').appendChild(card);
+        });
+      }
+      if(data.controls && Array.isArray(data.controls)) {
+        data.controls.forEach(function(controlData) {
+          var card = createCard(controlData, 'control');
+          document.getElementById('panel2').appendChild(card);
+        });
+      }
+      if(data.techniques && Array.isArray(data.techniques)) {
+        data.techniques.forEach(function(techniqueData) {
+          var card = createCard(techniqueData, 'technique');
+          document.getElementById('panel3').appendChild(card);
+        });
+      }
+    });
+  });
+  
